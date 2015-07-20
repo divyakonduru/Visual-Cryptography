0ca578f8452b170f95888c81e3ac77ad270ba9ad
bushj <Joel.H.Bush@Gmail.com>
2015-07-20 14:11:30 -0700

    Added Copyright to all source files

--- Visual Cryptography/src/justin_and_joel/ImageFunctions.java
+++ Visual Cryptography/src/justin_and_joel/ImageFunctions.java
@@ -1,3 +1,13 @@
+/*
+Visual Cryptography Project
+
+Copyright (c) 2015 Justin Blackmon and Joel Bush
+
+For licensing information refer to LICENSE.md
+
+This project is a Java application that utilizes visual cryptography techniques to encrypt and decrypt images.
+*/
+
 package justin_and_joel;
 
 import java.awt.Component;
